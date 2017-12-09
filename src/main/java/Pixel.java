3c050e1fa55126aefd582a23dc239493fcd3949e
Ruslan <zhd.ruslan@gmail.com>
2017-12-09 19:04:46 +0200

    [add Pixel description]
    Add pixel abstraction. THis class store coords.

---
+++ src/main/java/Pixel.java
@@ -0,0 +1,31 @@
+public class Pixel {
+    
+    private int x;
+    private int y;
+
+    /**
+     * Constructor.
+     * @param x the x
+     * @param y the y
+     */
+    public Pixel(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+    /**
+     * Gets x coordinate.
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Gets y coordinate.
+     * @return  y
+     */
+    public int getY() {
+        return y;
+    }
+}
+
