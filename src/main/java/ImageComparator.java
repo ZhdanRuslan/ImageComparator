6ce8c9ad0d1ddbea7b3a0d2a97101630e79be95e
Ruslan <zhd.ruslan@gmail.com>
2017-12-10 14:21:14 +0200

    [method which compare 2 pixels]

--- src/main/java/ImageComparator.java
+++ src/main/java/ImageComparator.java
@@ -1,3 +1,19 @@
 public class ImageComparator {
 
+    /**
+    * Method which compare two pixels
+    * @param firstPixel - first pixel
+    * @param secondPixel - second pixel
+     * */
+    private boolean pixelsDifference(int firstPixel, int secondPixel) {
+        int r1 = (firstPixel >> 16) & 255;
+        int g1 = (firstPixel >> 8) & 255;
+        int b1 = (firstPixel) & 255;
+        int r2 = (secondPixel >> 16) & 255;
+        int g2 = (secondPixel >> 8) & 255;
+        int b2 = (secondPixel) & 255;
+        int difference = (Math.abs(r1 - r2) + Math.abs(g1 - g2) + Math.abs(b1 - b2)) / 3 * 100;
+        return difference > 10;
+    }
+
 }
